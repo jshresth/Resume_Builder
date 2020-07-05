2119f74fb3c0074d7e0a6aee46aec7f48f3260bb
prashan07 <60672026+prashan07@users.noreply.github.com>
2020-07-04 21:09:45 -0400

    New changes

---
+++ education_action.php
@@ -0,0 +1,14 @@
+<?php
+include "connection.php";
+session_start();
+/*$schoolname = $_POST['sname']*/
+$cid=$_SESSION['cId'];
+$school= $_POST['school'];
+$city= $_POST['city'];
+$state= $_POST['state'];
+$study= $_POST['study'];
+
+$user_registration_query="insert into education(cid,name,city, state, course) values ('$cid','$school','$city','$state','$study')";
+$user_registration_result=mysqli_query($con,$user_registration_query) or die(mysqli_error($con));
+?>
+<meta http-equiv="refresh" content="3;url=education.html" />
