d1db0db81180a8fe385e9bc68e21d6f904801935
pratiksha16 <pratiksha.adhikari@wmich.edu>
2020-07-05 02:33:41 -0400

    db

--- Resume.sql
+++ Resume.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 04, 2020 at 11:16 PM
+-- Generation Time: Jul 05, 2020 at 08:31 AM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
@@ -33,11 +33,22 @@ CREATE TABLE `education` (
   `name` varchar(100) NOT NULL,
   `city` varchar(100) NOT NULL,
   `state` varchar(50) NOT NULL,
-  `degree` varchar(50) NOT NULL,
+  `degree` varchar(50) DEFAULT NULL,
   `course` varchar(50) NOT NULL,
-  `grad_date` date NOT NULL,
-  `attend` enum('present','not') NOT NULL
+  `grad_date` date DEFAULT NULL,
+  `attend` enum('present','not') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `education`
+--
+
+INSERT INTO `education` (`cid`, `name`, `city`, `state`, `degree`, `course`, `grad_date`, `attend`) VALUES
+(2, 'western', 'mic', 'ja', NULL, 'ba', NULL, NULL),
+(3, 'western', 'mi', 'mi', NULL, 'bh', NULL, NULL),
+(3, '', '', '', NULL, '', NULL, NULL),
+(4, 'WMU', 'Kalamazoo', 'MI', NULL, 'BS', NULL, NULL),
+(5, 'Western Michigan University', 'Kalamazoo', 'MI', NULL, 'BS', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -51,10 +62,23 @@ CREATE TABLE `job` (
   `title` varchar(50) NOT NULL,
   `city` varchar(50) NOT NULL,
   `state` varchar(10) NOT NULL,
-  `start_date` date NOT NULL,
-  `end_date` date NOT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
   `description` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `job`
+--
+
+INSERT INTO `job` (`cid`, `employer`, `title`, `city`, `state`, `start_date`, `end_date`, `description`) VALUES
+(3, 'paku', 'software', 'kalama', 'mi', NULL, NULL, ''),
+(4, 'wmu', 'sjd', 'nsj', 'ni', NULL, NULL, ''),
+(4, '', '', '', '', NULL, NULL, ''),
+(4, '', '', '', '', NULL, NULL, ''),
+(4, 'wmu', '', '', '', NULL, NULL, ''),
+(5, 'Google', 'Software Engineer', 'Newyork', 'NY', NULL, NULL, 'developing new software'),
+(5, '', '', '', '', NULL, NULL, '');
 
 -- --------------------------------------------------------
 
@@ -75,6 +99,17 @@ CREATE TABLE `personal_info` (
   `zcode` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `personal_info`
+--
+
+INSERT INTO `personal_info` (`cid`, `fname`, `lname`, `address`, `city`, `state`, `email`, `phone`, `profile`, `zcode`) VALUES
+(1, 'jksdjks', 'jsdkjs', 'sakjdsjhjska', 'lsjks', 'mi', 'p@gmail.com', '76473', 'iduuedhskjdssajdsakj', 67),
+(2, 'haha', 'haha', 'kjaa', 'hh', 'mi', 'h@gmail.com', '646', 'kjhjhj', 678),
+(3, 'hs', 'js', 'jksaksj', 'ms', 'mi', 'kjsskas@gmail.com', '7546', 'jhshjs', 408),
+(4, 'jyoti', 'shretsha', '839 w walnut', 'Kalamazoo', 'MI', 'shresthajyoti@gmail.com', '3526738813', 'hi Im doing great.', 10),
+(5, 'Jyoti', 'Shrestha', '839 W Walnut st', 'Kalamazoo', 'MI', 'shresthajyoti79@gmail.com', '6822265758', 'Looking for job.', 40007);
+
 -- --------------------------------------------------------
 
 --
@@ -86,6 +121,57 @@ CREATE TABLE `skills` (
   `skill` varchar(200) NOT NULL,
   `level` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `skills`
+--
+
+INSERT INTO `skills` (`cid`, `skill`, `level`) VALUES
+(3, 'git', ''),
+(3, 'team', ''),
+(3, 'git', ''),
+(3, 'git', ''),
+(3, 'team', ''),
+(3, 'git', ''),
+(3, 'git', ''),
+(3, 'team', ''),
+(3, 'git', ''),
+(3, 'git', ''),
+(3, 'team', ''),
+(3, 'git', ''),
+(3, 'his', ''),
+(3, 'sh', ''),
+(3, 'his', ''),
+(3, 'his', ''),
+(3, 'sh', ''),
+(3, 'his', ''),
+(3, 'his', ''),
+(3, 'sh', ''),
+(3, 'his', ''),
+(3, 'his', ''),
+(3, 'sh', ''),
+(3, 'his', ''),
+(3, '', ''),
+(3, '', ''),
+(3, '', ''),
+(3, 'SXa', 'Expert'),
+(3, 'Assad', 'Skilled'),
+(3, 'SXa', 'Expert'),
+(3, 'SXa', 'Expert'),
+(3, 'Assad', 'Skilled'),
+(3, 'SXa', 'Expert'),
+(3, 'hreagka', 'Beginner'),
+(3, 'hjsahjattetettete', 'Skilled'),
+(3, 'hreagka', 'Beginner'),
+(3, 'his', 'Beginner'),
+(3, 'jjas', 'Skilled'),
+(3, 'his', 'Beginner'),
+(4, 'team', 'Beginner'),
+(4, 'team', 'Skilled'),
+(4, 'team', 'Beginner'),
+(5, 'Java', 'Skilled'),
+(5, 'CSS', 'Expert'),
+(5, 'Java', 'Skilled');
 
 --
 -- Indexes for dumped tables
@@ -123,7 +209,7 @@ ALTER TABLE `skills`
 -- AUTO_INCREMENT for table `personal_info`
 --
 ALTER TABLE `personal_info`
-  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
