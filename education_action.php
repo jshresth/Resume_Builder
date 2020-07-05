2c2e2d01f15ef0b5b303c3da372f391ef6fbeae0
pratiksha16 <pratiksha.adhikari@wmich.edu>
2020-07-05 03:26:45 -0400

    final changes

--- education_action.php
+++ education_action.php
@@ -5,9 +5,11 @@ $cid= $_SESSION['cId'];
 $school= $_POST['school'];
 $city= $_POST['city'];
 $state= $_POST['state'];
+$degree=$_POST['edu_form'];
 $study= $_POST['study'];
+$grad_date= $_POST['date'];
 
-$user_registration_query="insert into education(cid,name,city, state,course) values ('$cid','$school','$city','$state','$study')";
+$user_registration_query="insert into education(cid,name,city, state,degree,course,grad_date) values ('$cid','$school','$city','$state','$degree','$study','$grad_date')";
 $user_registration_result=mysqli_query($con,$user_registration_query) or die(mysqli_error($con));
 ?>
 <meta http-equiv="refresh" content="3;url=skills.html" />
--- exp_action.php
+++ exp_action.php
@@ -8,10 +8,13 @@ $title= $_POST['title'];
 
 $city= $_POST['city'];
 $state= $_POST['state'];
+$start= $_POST['start'];
+$end= $_POST['end'];
+
 $desc= $_POST['desc'];
 
 
-$user_registration_query="insert into job(cid,employer,title,city,state,description) values ('$cid','$ename','$title','$city','$state','$desc')";
+$user_registration_query="insert into job(cid,employer,title,city,state,description,start_date, end_date) values ('$cid','$ename','$title','$city','$state','$desc','$start','$end')";
 $user_registration_result=mysqli_query($con,$user_registration_query) or die(mysqli_error($con));
 ?>
 <meta http-equiv="refresh" content="3;url=WilliamSexysphere.php "/>
