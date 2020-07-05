19cdb46a2fefb7d441ef649cb751903b94142bc7
prashan07 <60672026+prashan07@users.noreply.github.com>
2020-07-04 21:13:03 -0400

    Revert "New changes"
    This reverts commit 2119f74fb3c0074d7e0a6aee46aec7f48f3260bb.

--- connectsql.php
+++ connectsql.php
@@ -1,21 +1,7 @@
 <?php
     require 'connection.php';
     session_start();
-
-    $fname= $_POST['fname'];
-$lname= $_POST['lname'];
-$address= $_POST['add'];
-$city= $_POST['city'];
-$state= $_POST['state'];
-$zcode= $_POST['zcode'];
-$email= $_POST['email'];
-$phone= $_POST['phone'];
-$profile= $_POST['profile'];
-
-$user_registration_query="insert into personal_info(fname,lname,address,city,state,email,phone,profile,zcode) values ('$fname','$lname','$address','$city','$state','$email','$phone','$profile','$zcode')";
-$user_registration_result=mysqli_query($con,$user_registration_query) or die(mysqli_error($con));
-$_SESSION['cId']=mysqli_insert_id($con); 
+    
     
  ?>
- <meta http-equiv="refresh" content="3;url=education.html" />
 
