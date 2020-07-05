c6c509b070ed440cdfc6191c757d4fc4d0e4a86f
pratiksha16 <pratiksha.adhikari@wmich.edu>
2020-07-05 03:33:55 -0400

    new-database

--- Resume.sql
+++ Resume.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 05, 2020 at 08:31 AM
+-- Generation Time: Jul 05, 2020 at 09:32 AM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
@@ -35,7 +35,7 @@ CREATE TABLE `education` (
   `state` varchar(50) NOT NULL,
   `degree` varchar(50) DEFAULT NULL,
   `course` varchar(50) NOT NULL,
-  `grad_date` date DEFAULT NULL,
+  `grad_date` varchar(4) DEFAULT NULL,
   `attend` enum('present','not') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -48,7 +48,9 @@ INSERT INTO `education` (`cid`, `name`, `city`, `state`, `degree`, `course`, `gr
 (3, 'western', 'mi', 'mi', NULL, 'bh', NULL, NULL),
 (3, '', '', '', NULL, '', NULL, NULL),
 (4, 'WMU', 'Kalamazoo', 'MI', NULL, 'BS', NULL, NULL),
-(5, 'Western Michigan University', 'Kalamazoo', 'MI', NULL, 'BS', NULL, NULL);
+(5, 'Western Michigan University', 'Kalamazoo', 'MI', NULL, 'BS', NULL, NULL),
+(6, 'wetsern', 'jsjksa', 'jksjs', 'Undergraduate', 'computer ', '', NULL),
+(6, 'wetsern', 'jsjksa', 'jksjs', 'Undergraduate', 'computer ', '2021', NULL);
 
 -- --------------------------------------------------------
 
@@ -62,8 +64,8 @@ CREATE TABLE `job` (
   `title` varchar(50) NOT NULL,
   `city` varchar(50) NOT NULL,
   `state` varchar(10) NOT NULL,
-  `start_date` date DEFAULT NULL,
-  `end_date` date DEFAULT NULL,
+  `start_date` varchar(10) DEFAULT NULL,
+  `end_date` varchar(10) DEFAULT NULL,
   `description` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -78,7 +80,8 @@ INSERT INTO `job` (`cid`, `employer`, `title`, `city`, `state`, `start_date`, `e
 (4, '', '', '', '', NULL, NULL, ''),
 (4, 'wmu', '', '', '', NULL, NULL, ''),
 (5, 'Google', 'Software Engineer', 'Newyork', 'NY', NULL, NULL, 'developing new software'),
-(5, '', '', '', '', NULL, NULL, '');
+(5, '', '', '', '', NULL, NULL, ''),
+(6, 'wmu', 'tech', 'kala', 'MI', '2018', '2022', 'hgsghsghahJGS');
 
 -- --------------------------------------------------------
 
@@ -108,7 +111,8 @@ INSERT INTO `personal_info` (`cid`, `fname`, `lname`, `address`, `city`, `state`
 (2, 'haha', 'haha', 'kjaa', 'hh', 'mi', 'h@gmail.com', '646', 'kjhjhj', 678),
 (3, 'hs', 'js', 'jksaksj', 'ms', 'mi', 'kjsskas@gmail.com', '7546', 'jhshjs', 408),
 (4, 'jyoti', 'shretsha', '839 w walnut', 'Kalamazoo', 'MI', 'shresthajyoti@gmail.com', '3526738813', 'hi Im doing great.', 10),
-(5, 'Jyoti', 'Shrestha', '839 W Walnut st', 'Kalamazoo', 'MI', 'shresthajyoti79@gmail.com', '6822265758', 'Looking for job.', 40007);
+(5, 'Jyoti', 'Shrestha', '839 W Walnut st', 'Kalamazoo', 'MI', 'shresthajyoti79@gmail.com', '6822265758', 'Looking for job.', 40007),
+(6, 'Pratiksha', 'Adhikari', '839W Walnut St', 'Kalamazoo', 'Mi', 'prati@gmail.com', '6464095933', 'hjshsbhajghshjshjhhsajhshssjh', 49007);
 
 -- --------------------------------------------------------
 
@@ -171,7 +175,10 @@ INSERT INTO `skills` (`cid`, `skill`, `level`) VALUES
 (4, 'team', 'Beginner'),
 (5, 'Java', 'Skilled'),
 (5, 'CSS', 'Expert'),
-(5, 'Java', 'Skilled');
+(5, 'Java', 'Skilled'),
+(6, 'HTML', 'Novice'),
+(6, 'Team', 'Beginner'),
+(6, 'HTML', 'Novice');
 
 --
 -- Indexes for dumped tables
@@ -209,7 +216,7 @@ ALTER TABLE `skills`
 -- AUTO_INCREMENT for table `personal_info`
 --
 ALTER TABLE `personal_info`
-  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
